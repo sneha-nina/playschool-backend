691b9a5b1fbf88a4140bd6decec3b092e961432a
Sneha <snehasahu1485@gmail.com>
2025-08-27 20:08:52 +0530

    Fix controller and deploy

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello from PlaySchool Backend";
+    }
 
     @GetMapping("/hello")
     public String sayHello() {
